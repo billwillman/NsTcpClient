1e76a208cca416c8a0a1bb1ad3de90b307be40de
billwillman <billwillman@163.com>
2016-08-13 21:14:07 +0800

    【增加】NsTcpClient库

---
+++ Assets/tcp/ITcpClient.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+
+namespace NsTcpClient
+{
+
+	public enum eClientState
+	{
+		eCLIENT_STATE_NONE = 0,			
+		eClient_STATE_CONNECTING,	
+		eClient_STATE_CONNECTED,	
+		eClient_STATE_CONNECT_FAIL,	
+		eClient_STATE_ABORT,		
+		eClient_STATE_DISCONNECT,
+	};
+
+	public interface ITcpClient {
+		void Release();
+		bool Connect(string remoteIp, int remotePort, int timeOut = -1);
+		eClientState GetState();
+		bool Send(byte[] pData);
+		bool HasReadData();
+		int GetReadData(byte[] pBuffer, int offset);
+	}
+}
