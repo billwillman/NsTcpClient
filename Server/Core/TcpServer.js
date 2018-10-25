1db9214aa9cd71d01c93dbe0ebe32e63d658b3fc
billwillman <billwillman@qq.com>
2018-10-25 18:46:41 +0800

    【修改】服务器代码

--- Server/Core/TcpServer.js
+++ Server/Core/TcpServer.js
@@ -1,29 +1,105 @@
+/*
+ * 服务器TcpServer
+ * */
 
+var net = require("net");
 
-
-// 用户Session数据
-function UserSession(socket)
+// ---------------------------------类定义: TcpServer
+function TcpServer(bindPort)
 {
-    this.m_Socket = socket;
+    this.m_BindPort = bindPort;
 }
 
-UserSession.prototype.constructor = UserSession;
+// 构造函数
+TcpServer.prototype.constructor = TcpServer;
 
-// 关闭类型
-UserSession.prototype.EndType =
-{
-    "Server_End": 0
-}
-
-
-UserSession.prototype.Close = function ()
-{
-    if (this.m_Socket != null)
+// 获得绑定端口
+TcpServer.prototype.GetBindPort =
+    function()
     {
-        this.m_Socket.end(UserSession.Server_End);
-        this.m_Socket = null;
+        return this.m_BindPort;
     }
+
+TcpServer.prototype.GetAddress =
+    function()
+    {
+        if (this.m_Server != null)
+        {
+            return this.m_Server.address();
+        }
+        return null;
+    }
+
+TcpServer.prototype.Close =
+    function ()
+    {
+        if (this.m_Server != null)
+        {
+            // 关闭连接
+            this.m_Server.close();
+            this.m_Server = null;
+        }
+    }
+
+// 产生链接时调用
+TcpServer.prototype._OnConnected = function (socket)
+{
+    if (socket != null)
+    {
+        socket.setNoDelay(true);
+    }
+
+    console.log("_OnConnected");
 }
 
+// 侦听时候
+TcpServer.prototype._OnListening = function ()
+{
+    console.log("_OnListening");
+}
 
-module.exports = UserSession;
+// 出错
+TcpServer.prototype._OnError = function ()
+{
+    console.log("_OnError");
+}
+
+TcpServer.prototype._OnDisconnect = function ()
+{
+    console.log("_OnDisconnect");
+}
+
+TcpServer.prototype._OnPacketRead = function (data)
+{
+    console.log("_OnPacketRead");
+}
+
+TcpServer.prototype._OnClose = function ()
+{
+    console.log("_OnClose");
+}
+
+// 开始接受网络连接，返回值：是否成功监听
+TcpServer.prototype.Accept =
+    function ()
+    {
+        this.Close();
+        if (this.m_BindPort == null)
+            return false;
+
+        var server = net.createServer();
+        this.m_Server = server;
+        server.on("connection", this._OnConnected);
+        server.on("listening", this._OnListening);
+        server.on("error", this._OnError);
+        server.on("data", this._OnPacketRead);
+        server.on("end", this._OnDisconnect);
+        server.on("close", this._OnClose);
+        // 侦听事件
+        server.listen(this.m_BindPort);
+        return true;
+    }
+//---------------------------------------------------
+
+
+module.exports = TcpServer;
--- Server/User/UserSession.js
+++ Server/User/UserSession.js
@@ -9,18 +9,12 @@ function UserSession(socket)
 
 UserSession.prototype.constructor = UserSession;
 
-// 关闭类型
-UserSession.prototype.EndType =
-{
-    "Server_End": 0
-}
-
 
 UserSession.prototype.Close = function ()
 {
     if (this.m_Socket != null)
     {
-        this.m_Socket.end(UserSession.Server_End);
+        this.m_Socket.destroy();
         this.m_Socket = null;
     }
 }
