390ae4c4718c4ff0f7fd5a243d96ccab7784a650
billwillman <billwillman@qq.com>
2018-11-06 14:36:35 +0800

    【增加】INITBUF

--- Server/Core/AbstractClientMessage.js
+++ Server/Core/AbstractClientMessage.js
@@ -28,8 +28,8 @@ AbstractClientMessage.prototype.DoSend =
 AbstractClientMessage.prototype.WriteByte =
     function (byte)
     {
-        _InitBuf();
-        this.m_Buf.fill(byte, this.m_BufOffset, 1);
+        this._InitBuf();
+        this.m_Buf.writeUInt8(byte, this.m_BufOffset);
         this.m_BufOffset++;
     }
 
@@ -43,7 +43,7 @@ AbstractClientMessage.prototype.WriteBool =
 AbstractClientMessage.prototype.WriteInt =
     function (int)
     {
-        _InitBuf();
+        this._InitBuf();
         this.m_Buf.writeInt32LE(int, this.m_BufOffset);
         this.m_BufOffset += 4;
     }
@@ -51,7 +51,7 @@ AbstractClientMessage.prototype.WriteInt =
 AbstractClientMessage.prototype.WriteUInt =
     funciton (uint)
     {
-        _InitBuf();
+        this._InitBuf();
         this.m_Buf.writeUInt32LE(uint, this.m_BufOffset);
         this.m_BufOffset += 4;
     }
@@ -59,7 +59,7 @@ AbstractClientMessage.prototype.WriteUInt =
 AbstractClientMessage.prototype.WriteInt64 =
     function (int64)
     {
-        _InitBuf();
+        this._InitBuf();
         this.m_Buf.writeIntLE(int64, this.m_BufOffset, 6);
         this.m_BufOffset += 6;
     }
@@ -68,11 +68,43 @@ AbstractClientMessage.prototype.WriteInt64 =
 AbstractClientMessage.prototype.WriteString =
     function (str)
     {
-        _InitBuf();
+        this._InitBuf();
         // 字符串长度
         this.WriteUInt(str.byteLength);
         this.m_Buf.write(str, this.m_BufOffset, str.length, "utf-8");
         this.m_BufOffset += str.byteLength;
+    }
+
+AbstractClientMessage.prototype.WriteUInt16 =
+    function (uint16)
+    {
+        this._InitBuf();
+        this.m_Buf.writeUInt16LE(uint16, this.m_BufOffset);
+        this.m_BufOffset += 2;
+    }
+
+AbstractClientMessage.prototype.WriteInt16 =
+    function (int16)
+    {
+        this._InitBuf();
+        this.m_Buf.writeInt16LE(int16, this.m_BufOffset);
+        this.m_BufOffset += 2;
+    }
+
+AbstractClientMessage.prototype.WriteFloat =
+    function (float)
+    {
+        this._InitBuf();
+        this.m_Buf.writeFloatLE(float, this.m_BufOffset);
+        this.m_BufOffset += 4;
+    }
+
+AbstractClientMessage.prototype.WriteDouble =
+    function (double)
+    {
+        this._InitBuf();
+        this.m_Buf.writeDoubleLE(double, this.m_BufOffset);
+        this.m_BufOffset += 8;
     }
 
 
