74373490777fc6c47fd4cfc7b003e369654c79a3
billwillman <billwillman@qq.com>
2018-11-06 15:00:56 +0800

    【增加】AbstractServerMessage.js

--- Server/Core/AbstractServerMessage.js
+++ Server/Core/AbstractServerMessage.js
@@ -1,13 +1,34 @@
 /*
 服务器协议
 */
+var IServerMessagListener = require("./IServerMessagListener");
 
-function AbsractServerMessage(buf)
-{
-    this.m_Buf = buf;
-}
+function AbsractServerMessage()
+{}
 
+AbsractServerMessage.prototype = IServerMessagListener.prototype;
 AbsractServerMessage.prototype.constructor = AbsractServerMessage;
+
+AbsractServerMessage.prototype.OnMessage = 
+    function (packet, clientSocket, netMgr)
+    {
+        this.m_HeaderId = packet.header.header;
+        this.m_Buf = packet.data;
+        this.m_NetMgr = netMgr;
+    }
+
+AbsractServerMessage.prototype.GetHeaderId =
+    function ()
+    {
+        return this.m_HeaderId;
+    }
+
+// 接收继承方法
+AbsractServerMessage.prototype.DoRecv =
+    function ()
+    {
+        
+    }
 
 
 module.exports = AbsractServerMessage;
