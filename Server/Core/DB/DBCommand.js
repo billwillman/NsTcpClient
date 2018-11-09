6cdacc680b2c8aaa7773fc1fccb6b5a98a113594
billwillman <billwillman@qq.com>
2018-11-09 14:08:13 +0800

    【增加】DBCOMMAND

--- Server/Core/DB/DBCommand.js
+++ Server/Core/DB/DBCommand.js
@@ -5,5 +5,38 @@
 // DB命令
 class DBCommand
 {
-    
+    constructor(key, sql, paramArgs)
+    {
+        this.m_Key = key;
+        this.m_sql = sql;
+        this.m_Node = null;
+        this.m_ParamArgs = paramArgs;
+    }
+
+    AttachLinkedNode(linkedNode)
+    {
+        this.m_Node = linkedNode;
+    }
+
+    ChangeParamArgs(paramArgs)
+    {
+        this.m_ParamArgs = paramArgs;
+    }
+
+    GetKey()
+    {
+        return this.m_Key;
+    }
+
+    GetCommand()
+    {
+        return this.m_sql;
+    }
+
+    GetLinkedNode()
+    {
+        return this.m_Node;
+    }
 }
+
+module.exports = DBCommand;
--- Server/Core/DBServer.js
+++ Server/Core/DBServer.js
@@ -5,12 +5,33 @@
 var NetManager = require("./NetManager");
 var DefaultPacketHandler = require("./DefaultPacketHandler");
 var RegisterDBMessage = require("./RegisterDBMessage");
+var LinkedList = require("./struct/LinkedList");
+var Dictionary = require("./struct/Dictionary");
+var DBCommand = require("./DB/DBCommand");
 
 class DBServer extends NetManager
 {
     constructor(port)
     {
        super();
+       this.m_CommandList = new LinkedList();
+       this.m_CommandMap = new Dictionary();
+    }
+
+    // 推入參數隊列
+    Push(key, sql, paramArgs)
+    {
+       var command = this.m_CommandMap.GetValue(key);
+       if (command == null)
+       {
+          command = new DBCommand(key, sql, paramArgs);
+          var linkedNode = this.m_CommandList.AddLastValue(command);
+          command.AttachLinkedNode(linkedNode);
+          this.m_CommandMap.Add(key, command);
+       } else
+       {
+          command.ChangeParamArgs(paramArgs);
+       }
     }
 }
 
