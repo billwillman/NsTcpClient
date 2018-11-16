15917998e5157881c866e6e9f71f27e9b1471dea
billwillman <billwillman@163.com>
2018-11-17 01:44:21 +0800

    【修正】报错

--- Server/Core/UdpServer.js
+++ Server/Core/UdpServer.js
@@ -3,7 +3,7 @@ const dgram = require('dgram');
 
 class UdpServer
 {
-    constructor(packetHandleClass)
+    constructor(packetHandleClass, recvBufSize)
     {
         this.m_Socket = null;
         this.m_Listener = null;
