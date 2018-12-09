2790c4ebfd572dbdc8bafca5d83647e4a929f117
billwillman <billwillman@163.com>
2018-12-09 15:49:07 +0800

    【增加】UNITY 新寻路测试代码

---
+++ Assets/Recast/script/NavMeshAgent.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using org.critterai.nav;
+using org.critterai.u3d;
+
+namespace Recast
+{
+    public class NavMeshAgent: MonoBehaviour
+    {
+    }
+}
---
+++ Assets/Recast/script/NavMeshMap.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using org.critterai.nav;
+using org.critterai.u3d;
+
+namespace Recast
+{
+    public class NavMeshMap : MonoBehaviour
+    {
+        private Navmesh m_NavMesh = null;
+        private static NavMeshMap m_Map = null;
+
+        void OnDestroy()
+        {
+            Clear();
+        }
+
+        public static bool LoadMap(byte[] buffer)
+        {
+            NavMeshMap map = GetInstance();
+            return map.LoadMapFromBuffer(buffer);
+        }
+
+        public static NavMeshMap GetInstance()
+        {
+            if (m_Map == null)
+            {
+                GameObject gameObj = new GameObject("NavMeshMap", typeof(NavMeshMap));
+                m_Map = gameObj.GetComponent<NavMeshMap>();
+            }
+            return m_Map;
+        }
+
+        private void Clear()
+        {
+            if (m_NavMesh != null)
+            {
+                m_NavMesh.RequestDisposal();
+                m_NavMesh = null;
+            }
+        }
+
+        private bool LoadMapFromBuffer(byte[] buffer)
+        {
+            Clear();
+            if (buffer == null || buffer.Length <= 0)
+                return false;
+
+            try
+            {
+                var status = Navmesh.Create(buffer, out m_NavMesh);
+                var ret = NavUtil.Succeeded(status) && m_NavMesh != null;
+                if (!ret)
+                    Clear();
+                return ret;
+            } catch
+            {
+                return false;
+            }
+        }
+    }
+}
