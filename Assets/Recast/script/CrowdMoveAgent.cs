2ac50759ceee02bad40eabf269233fd31f80184b
billwillman <billwillman@qq.com>
2018-12-12 21:37:03 +0800

    【修改】移动

---
+++ Assets/Recast/script/CrowdMoveAgent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using org.critterai.nav;
+using org.critterai.u3d;
+
+namespace Recast
+{
+    // 跟随对象
+    public class CrowdMoveAgent : MonoBehaviour
+    {
+        private CrowdAgent m_Agent = null;
+
+        public CrowdMoveMgr m_Mgr = null;
+
+        internal CrowdAgent Agent
+        {
+            get
+            {
+                return m_Agent;
+            }
+        }
+
+        private void ClearAgent()
+        {
+            if (m_Agent != null)
+            {
+                if (m_Mgr != null)
+                {
+                    m_Mgr.RemoveAgent(this);
+                }
+                m_Agent = null;
+            }
+        }
+
+        void OnDestroy()
+        {
+            ClearAgent();
+        }
+    }
+}
--- Assets/Recast/script/CrowdMoveMgr.cs
+++ Assets/Recast/script/CrowdMoveMgr.cs
@@ -5,13 +5,32 @@ using org.critterai.u3d;
 namespace Recast
 {
     // 群跟随管理器
-    public class CrowdMgr: MonoBehaviour
+    public class CrowdMoveMgr: MonoBehaviour
     {
         private CrowdManager m_Mgr = null;
 
         public int m_MaxAgent = 100;
         public float m_MaxAgentRadius = 45f;
         public Navmesh m_NavMesh = null;
+        private bool m_IsAppQuit = false;
+
+        void OnApplicationQuit()
+        {
+            m_IsAppQuit = true;
+        }
+
+        public void RemoveAgent(CrowdMoveAgent agent)
+        {
+            if (m_IsAppQuit)
+                return;
+
+            if (agent == null)
+                return;
+            if (m_Mgr != null)
+            {
+                m_Mgr.RemoveAgent(agent.Agent);
+            }
+        }
 
         public void Init(Navmesh navMesh, int maxAgent = 100, float maxAgentRaidus = 45f)
         {
@@ -21,10 +40,10 @@ namespace Recast
             Init();
         }
 
-        public static CrowdMgr Create(Navmesh navMesh, int maxAgent = 100, float maxAgentRaidus = 45f)
+        public static CrowdMoveMgr Create(Navmesh navMesh, int maxAgent = 100, float maxAgentRaidus = 45f)
         {
-            GameObject gameObj = new GameObject("CrowdMgr", typeof(CrowdMgr));
-            CrowdMgr mgr = gameObj.GetComponent<CrowdMgr>();
+            GameObject gameObj = new GameObject("CrowdMoveMgr", typeof(CrowdMoveMgr));
+            CrowdMoveMgr mgr = gameObj.GetComponent<CrowdMoveMgr>();
             mgr.Init(navMesh, maxAgent, maxAgentRaidus);
             return mgr;
         }
