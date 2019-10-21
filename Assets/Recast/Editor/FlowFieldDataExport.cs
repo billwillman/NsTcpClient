80d50588acd9078924f446db7c60446c84c2ae8f
billwillman <billwillman@qq.com>
2019-10-21 20:32:46 +0800

    【增加】FlowFieldDataExport.cs

---
+++ Assets/Recast/Editor/FlowFieldDataExport.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class FlowFieldDataExport : EditorWindow {
+
+	public FlowFieldDataExport() {
+        this.titleContent = new GUIContent("FlowField导出工具");
+    }
+
+    [MenuItem("FlowField/导出...")]
+    public static void CreateWindow() {
+        EditorWindow.GetWindow(typeof(FlowFieldDataExport));
+    }
+}
