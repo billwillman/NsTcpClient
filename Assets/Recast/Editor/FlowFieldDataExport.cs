50026351a3b3c55efc9d1acd5871eb909641c6f7
billwillman <billwillman@qq.com>
2019-10-21 20:41:58 +0800

    【增加】

--- Assets/Recast/Editor/FlowFieldDataExport.cs
+++ Assets/Recast/Editor/FlowFieldDataExport.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using UnityEditor;
 using System.Collections;
+using org.critterai.nav;
+using org.critterai.u3d;
 
 public class FlowFieldDataExport : EditorWindow {
 
 	public FlowFieldDataExport() {
         this.titleContent = new GUIContent("FlowField导出工具");
+       
+
     }
 
     [MenuItem("FlowField/导出...")]
